ea700428094abc7e764de5305d0416f09050a6e6
VIGNESH <130775921+Vigneshven@users.noreply.github.com>
2023-10-03 09:51:04 +0530

    Update jsobjectusingclass.js

--- modules2/jsobjectusingclass.js
+++ modules2/jsobjectusingclass.js
@@ -11,8 +11,8 @@ class person {
 
     }
 }
-const p1 =new person ("yeswanth","reddy");
-const p2 = new person("leo","das");
+const p1 =new person ("vignesh","v");
+const p2 = new person("viki","vignesh");
 
 console.log(p1);
 console.log(p2);
